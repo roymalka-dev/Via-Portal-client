10829323de8dc93253716389c0b8724b4c55b616
Roy Malka <roy.malka@roy.malka-MBP>
2024-06-27 12:11:48 +0300

    ap config

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,11 +1,25 @@
+const env = import.meta.env.VITE_APP_ENV || process.env.VITE_APP_ENV;
+
 const googleClientId =
   import.meta.env.VITE_APP_GOOGLE_CLIENT_ID ||
   process.env.VITE_APP_GOOGLE_CLIENT_ID;
 
+const devBaseUrl =
+  import.meta.env.VITE_APP_DEV_BASE_URL || process.env.VITE_APP_DEV_BASE_URL;
+
+const prodBaseUrl =
+  import.meta.env.VITE_APP_PROD_BASE_URL || process.env.VITE_APP_PROD_BASE_URL;
+
+const devApiBaseUrl =
+  import.meta.env.VITE_APP_DEV_API_BASE_URL ||
+  process.env.VITE_APP_DEV_API_BASE_URL;
+const prodApiBaseUrl =
+  import.meta.env.VITE_APP_PROD_BASE_URL || process.env.VITE_APP_PROD_BASE_URL;
+
 const appConfig = {
   apiPrefix: "/api",
-  baseUrl: "http://localhost:3000",
-  apiBaseUrl: "http://localhost:3000",
+  baseUrl: env === "DEV" ? devBaseUrl : prodBaseUrl,
+  apiBaseUrl: env === "DEV" ? devApiBaseUrl : prodApiBaseUrl,
   googleClientId: googleClientId || "client-id",
   authenticatedEntryPath: "/",
   unAuthenticatedEntryPath: "/auth/login",
