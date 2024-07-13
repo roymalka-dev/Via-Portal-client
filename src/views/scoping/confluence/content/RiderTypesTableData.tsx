bb8f1d906aeb5bac7c42e13faf2ac82d6c844467
Roy Malka <roy.malka@roy.malka-MBP>
2024-07-13 10:15:27 +0300

    rider types fix

--- src/views/scoping/confluence/content/RiderTypesTableData.tsx
+++ src/views/scoping/confluence/content/RiderTypesTableData.tsx
@@ -6,20 +6,19 @@ import { scopingBadges } from "../elements/badge";
 export const generateRiderTypesTableData = (
   configurations: CityConfigurations
 ): TableData => {
-  //const riderTypesArray = [""]; //configurations.rider_types ?? [""];
+  const riderTypesArray = configurations?.rider_types?.toString() ?? [""];
   return {
     headline: `Rider Types ${
       scopingBadges.upgradeTeam + " " + scopingBadges.mandatory
     }`,
     headers: ["Rider type", "Description", "Max passengers", "Price"],
-    rows: [configurations.rider_types || ""],
-    /*
+    //rows: [configurations.rider_types?.toString() || "", "", "", ""],
+
     rows: riderTypesArray?.map((riderType: any) => [
       riderType.type || "",
       riderType.description || "",
       riderType.maxPassengers || "",
       riderType.price || "",
     ]),
-    */
   };
 };
