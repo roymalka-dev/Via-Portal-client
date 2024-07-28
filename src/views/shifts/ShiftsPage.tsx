f166d2f1b9dc6bede28c59cae5c795cd18df5b80
Roy Malka <roy.malka@roy.malka-MBP>
2024-07-28 18:04:24 +0300

    shift valdiations

--- src/views/shifts/ShiftsPage.tsx
+++ src/views/shifts/ShiftsPage.tsx
@@ -1,129 +1,129 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from "react";
 import { TextField, Button, Box, CircularProgress } from "@mui/material";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import ApiService from "@/services/ApiService";
 
-const ShiftsPage: React.FC = () => {
-  const [startTime, setStartTime] = useState<string>(
-    new Date().toISOString().slice(0, 19)
-  );
-  const [endTime, setEndTime] = useState<string>(
-    new Date().toISOString().slice(0, 19)
-  );
-  const [responseText, setResponseText] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+const validationSchema = Yup.object({
+  startTime: Yup.date()
+    .required("Start time is required")
+    .max(Yup.ref("endTime"), "Start time must be before end time"),
+  endTime: Yup.date()
+    .required("End time is required")
+    .min(Yup.ref("startTime"), "End time must be after start time")
+    .test(
+      "duration",
+      "The time range must be at least one hour",
+      function (value) {
+        const startTime = new Date(this.parent.startTime);
+        const endTime = new Date(value);
+        return endTime.getTime() - startTime.getTime() >= 3600000; // 3600000 milliseconds = 1 hour
+      }
+    ),
+});
 
-  const handleStartTimeChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setStartTime(event.target.value);
-  };
-
-  const handleEndTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEndTime(event.target.value);
-  };
-
-  const handleSendRequest = async () => {
-    const formattedStartTime = startTime + "Z";
-    const formattedEndTime = endTime + "Z";
-    setIsLoading(true);
+const ShiftsPage = () => {
+  const handleSendRequest = async (values: any, actions: any) => {
+    const formattedStartTime = `${values.startTime}:10Z`;
+    const formattedEndTime = `${values.endTime}:10Z`;
+    actions.setSubmitting(true);
     try {
       const response = await ApiService.get(
         `shift/get-google-calendar-shifts?timeMin=${formattedStartTime}&timeMax=${formattedEndTime}`
       );
       const formattedResponse = formatResponse(response);
-      setResponseText(
+      actions.setStatus(
         `Hey everyone,
-Our next 2.0 upgrade will take place on <date> for <tenant>-<id> TaaS/SaaS OD/PB city, starting at 06:30.
-Tagging the shifts in case any issue will be raised, please approve your availability /reply with :+1::skin-tone-2: . Thanks a lot in advance!
-Good luck! :viablueheart:\n\n` + formattedResponse
+Our next 2.0 upgrade will take place on tomorrow Sunday for your city, starting at 06:30.
+Tagging the shifts in case any issue will be raised, please approve your availability. Thanks a lot in advance!
+Good luck!\n\n` + formattedResponse
       );
     } catch (error) {
       console.error("Error fetching data:", error);
-      setResponseText("Failed to fetch data");
+      actions.setStatus("Failed to fetch data");
     } finally {
-      setIsLoading(false);
+      actions.setSubmitting(false);
     }
   };
+
   const formatResponse = (data: any) => {
     return Object.entries(data)
       .map(([category, shifts]) => {
-        // Flatten the names across all shifts in the category
-        const allNames = Object.values(shifts as { [key: string]: string[] })
-          .flat() // Flattens the array of names from all schedules
-          .map((name) => `@ ${name}`) // Prefix each name with '@'
+        const allNames = Object.values(shifts as { [s: string]: unknown })
+          .flat()
+          .map((name) => `@${name}`)
           .join(", ");
         return `${category}: ${allNames}`;
       })
       .join("\n\n");
   };
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(responseText).then(
-      () => {
-        alert("Copied to clipboard!");
-      },
-      (err) => {
-        console.error("Could not copy text: ", err);
-      }
-    );
-  };
-
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        p: 4,
+    <Formik
+      initialValues={{
+        startTime: new Date().toISOString().slice(0, 16),
+        endTime: new Date().toISOString().slice(0, 16),
       }}
+      validationSchema={validationSchema}
+      onSubmit={handleSendRequest}
     >
-      <Box sx={{ display: "flex", gap: 1 }}>
-        <TextField
-          type="datetime-local"
-          label="Start Time"
-          value={startTime}
-          onChange={handleStartTimeChange}
-          sx={{ my: 2, width: 250 }}
-          InputLabelProps={{ shrink: true }}
-        />
-        <TextField
-          type="datetime-local"
-          label="End Time"
-          value={endTime}
-          onChange={handleEndTimeChange}
-          sx={{ my: 2, width: 250 }}
-          InputLabelProps={{ shrink: true }}
-        />
-        <Button
-          variant="outlined"
-          onClick={handleSendRequest}
-          sx={{ my: 2, position: "relative" }}
-        >
-          {isLoading ? (
-            <CircularProgress size={24} sx={{ position: "absolute" }} />
-          ) : (
-            "Fetch Shifts"
-          )}
-        </Button>
-      </Box>
-
-      <TextField
-        label="Response"
-        multiline
-        fullWidth
-        disabled
-        minRows={10}
-        value={responseText}
-        variant="outlined"
-        sx={{ width: "100%", minHeight: 300, mt: 2 }}
-      />
-      <Button onClick={copyToClipboard} variant="outlined" sx={{}}>
-        Copy to Clipboard
-      </Button>
-    </Box>
+      {({ isSubmitting, status, touched, errors }) => (
+        <Form>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              p: 4,
+            }}
+          >
+            <Box sx={{ display: "flex", gap: 2, marginBottom: 2 }}>
+              <Field
+                as={TextField}
+                type="datetime-local"
+                name="startTime"
+                label="Start Time"
+                helperText={<ErrorMessage name="startTime" />}
+                error={touched.startTime && !!errors.startTime}
+                sx={{ width: 250 }}
+              />
+              <Field
+                as={TextField}
+                type="datetime-local"
+                name="endTime"
+                label="End Time"
+                helperText={<ErrorMessage name="endTime" />}
+                error={touched.endTime && !!errors.endTime}
+                sx={{ width: 250 }}
+              />
+            </Box>
+            <Button
+              type="submit"
+              variant="outlined"
+              disabled={isSubmitting}
+              sx={{ my: 2, position: "relative" }}
+            >
+              {isSubmitting ? (
+                <CircularProgress size={24} sx={{ position: "absolute" }} />
+              ) : (
+                "Fetch Shifts"
+              )}
+            </Button>
+            <TextField
+              label="Response"
+              multiline
+              fullWidth
+              disabled
+              minRows={10}
+              value={status || ""}
+              variant="outlined"
+              sx={{ width: "100%", minHeight: 300, mt: 2 }}
+            />
+          </Box>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
